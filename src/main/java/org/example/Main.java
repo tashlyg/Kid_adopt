882fabf622d6b9297308873ef829053ba7da666a
tashlyg <tashlygg@gmail.com>
2025-12-11 17:47:47 +0500

    no more sex

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,37 @@ package org.example;
 
 class kid{
     int age;
-    String sex;
+    String gender;
+    String disease;
 }
 class adopter{
     int age;
-    String sex;
+    String gender;
 }
 class shelter{
     String name;
+    String location;
 }
 
 public class Main {
     static void main() {
-        IO.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            IO.println("i = " + i);
-        }
+        IO.println("Hello world!");
+
+        adopter adopter1 = new adopter();
+        adopter adopter2 = new adopter();
+
+        kid kid1 = new kid();
+        kid kid2 = new kid();
+        kid kid3 = new kid();
+
+        shelter shelter1 = new shelter();
+
+        adopter1.age = 30;
+        adopter1.gender = "Female";
+        adopter1.age = 30;
+        adopter1.gender = "Male";
+
+        int i = 1;
+        IO.println("i = " + i);
     }
 }

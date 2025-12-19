f66d2594bcb5c9630b7b2d7242172961b82382f7
tashlyg <tashlygg@gmail.com>
2025-12-20 00:31:56 +0500

    finalized the class employer

--- src/main/java/org/example/Employer.java
+++ src/main/java/org/example/Employer.java
@@ -6,5 +6,43 @@ public class Employer {
     String gender;
     int salary;
     String shelter;
+    public Employer(String name, int age, String gender, int salary, String shelter){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.shelter = shelter;
+    }
+    public Employer(){
+        this.name = "unknown";
+        this.age = -1;
+        this.gender = "unknown";
+        this.salary = -1;
+        this.shelter = "unknown";
+    }
+    void setName(String name){
+        this.name = name;
+    }
+    void setSalary(int salary){
+        this.salary = salary;
+    }
+    void setShelter(String shelter){
+        this.shelter = shelter;
+    }
 
+    String getName(){
+        return this.name;
+    }
+    int getAge(){
+        return this.age;
+    }
+    String getGender(){
+        return this.gender;
+    }
+    int setSalary(){
+        return this.salary;
+    }
+    String setShelter(){
+        return this.shelter;
+    }
 }
